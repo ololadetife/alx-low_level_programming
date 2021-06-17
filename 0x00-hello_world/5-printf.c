d49924997c5523f78cc08d31cfd1262886d79ff2
ololadetife <akintolamayode1@gmail.com>
2021-06-17 07:11:17 -0700

    Low level Programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include < stdio.h >
+#include < string.h >
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
