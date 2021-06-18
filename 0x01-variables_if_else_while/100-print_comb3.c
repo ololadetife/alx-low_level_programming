cb4da4977f4db90c5ebc3d3358ef5eaf6c83843b
ololadetife <akintolamayode1@gmail.com>
2021-06-18 12:06:18 -0700

    Low Level Programming

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,28 @@
 */
 int main(void)
 {
-int number1 = 0;
-int number2;
-int comma = ',';
-while (number1 < number2 && number2 <= 9)
+int  i;
+int j;
+for (i = 48 ; i < 58 ; i++)
 {
-putchar(number2++);
-putchar(number1++);
-putchar(comma);
+for (j = i ; j < 58 ; j++)
+{
+if (i == j)
+{
+continue;
+}
+putchar(i);
+putchar(j);
+if (i == 56 && j == 57)
+{
+break;
+}
+else
+{
+putchar(',');
 putchar(' ');
+}
+}
 }
 putchar('\n');
 return (0);
