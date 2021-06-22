e237869b0af14016c1f0d1200d91fbf4c189413a
ololadetife <akintolamayode1@gmail.com>
2021-06-22 06:10:47 -0700

    Functions and Nested Loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* main - Entry point
+*/
+void print_alphabet(void)
+{
+char character;
+for (character = 'a' ; character <= 'z' ; character++)
+{
+_putchar(character);
+_putchar('\n');
+}
