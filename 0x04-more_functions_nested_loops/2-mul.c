8a182ff95f193a40ef1b2dc3b76f681971fafc47
ololadetife <53858116+ololadetife@users.noreply.github.com>
2021-06-24 19:55:23 +0100

    More functions and nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+* mul - multiplies a and b
+* @a: integer to be tested
+* @b: integer to be tested
+* Return: 0
+*/
+
+int mul(int a, int b)
+{
+int mul;
+mul = a * b;
+_putchar(mul);
+return (0);
+}
