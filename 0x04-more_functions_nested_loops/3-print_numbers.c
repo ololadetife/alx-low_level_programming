bb59c080367089225110de052f29ea31311408a2
ololadetife <53858116+ololadetife@users.noreply.github.com>
2021-06-24 12:38:52 -0700

    more functions and nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,14 @@
-#include "holberton.h"                                                                                                                  
-                                                                                                                                        
-/**                                                                                                                                     
-* print_numbers - a function that prints the numbers, from 0 to 9                                                                                
-*                                                                                                                                       
-* Return:0-9 followed by newline                                                                                                                 
-*/                                                                                                                                      
-                                                                                                                                        
-void print_numbers(void)                                                                                                                
-{                                                                                                                                       
-int i;                                                                                                                                  
-for (i = 0 ; i <= 9 ; i++)                                                                                                                                                                                                                                                
-_putchar(i + '0');                                                                                                                                                                                                                                                               
-_putchar('\n');                                                                                                                         
-}                                                                                                                                       
+#include "holberton.h"
+
+/**
+ * print_numbers - print 0-9
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar(i + 48);
+	_putchar('\n');
+}                                                                                                                                    
     
