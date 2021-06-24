9f0157f3cea1ad6bd228c316802ef25b14b7d7f9
ololadetife <53858116+ololadetife@users.noreply.github.com>
2021-06-24 12:40:57 -0700

    header

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,15 +1,25 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-/* checks for uppercase character */
-int _isupper(int c);
-
-/* checks if c is digit */
-int _isdigit(int c);
-
-/* multiplies a and b */
-int mul(int a, int b);
-
-/* print numbers */
+/* 3-print_numbers.c */
 void print_numbers(void);
+
+/* 4-print_most_numbers.c */
+void print_most_numbers(void);
+
+/* 5-more_numbers.c */
+void more_numbers(void);
+
+/* 6-print_line.c */
+void print_line(int n);
+
+/* 7-print_diagonal.c */
+void print_diagonal(int);
+
+/* 8-print_square.c */
+void print_square(int size);
+
+/* 10-print-triangle.c */
+void print_triangle(int size);
+
+/* 101-print_number.c */
+void print_number(int n);
+
 #endif
