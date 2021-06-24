674f058a5e7be805bb29f50cbd009baccbc4044b
ololadetife <53858116+ololadetife@users.noreply.github.com>
2021-06-24 12:30:33 -0700

    More functions and nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"                                                                                                                  
+                                                                                                                                        
+/**                                                                                                                                     
+* print_numbers - prints numbers between 0 and 9                                                                                        
+*                                                                                                                                       
+* Return: 0                                                                                                                             
+*/                                                                                                                                      
+                                                                                                                                        
+void print_numbers(void)                                                                                                                
+{                                                                                                                                       
+int a;                                                                                                                                  
+for (a >= 0 ; a <= 9 ; a++)                                                                                                             
+{                                                                                                                                       
+_putchar(a);                                                                                                                            
+}                                                                                                                                       
+_putchar('\n');                                                                                                                         
+}                                                                                                                                       
+    
