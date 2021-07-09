fd1c12847326ceda6c8ae6b5a189b142f007c6eb
ololadetife <53858116+ololadetife@users.noreply.github.com>
2021-07-09 14:31:53 +0100

    Static Library

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,19 +1,13 @@
 #include "holberton.h"
 
 /**
-* _isupper - returns 1 if c is uppercase
-* @c: integer to be tested
-* Return: 1 or 0
-*/
+ * _isupper - check for upper case letter
+ * @c : character to check the case
+ * Return:0 or 1
+ */
 
 int _isupper(int c)
 {
-if (c >= 65 && c <= 90)
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	return (c >= 65 && c <= 90);
+
 }
