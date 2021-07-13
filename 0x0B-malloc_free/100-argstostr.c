ee646986425e6836ee4d394641c94d0d446c2d52
ololadetife <akintolamayode1@gmail.com>
2021-07-13 12:31:04 -0700

    Malloc, Free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,7 +7,6 @@
  * Return: int
  */
 
-
 int _strlen(char *s)
 {
 int size = 0;
@@ -29,24 +28,19 @@ int i = 0, nc = 0, j = 0, cmpt = 0;
 char *s;
 
 if (ac == 0 || av == NULL)
-	return (NULL);
-
+return (NULL);
 for (; i < ac; i++, nc++)
-	nc += _strlen(av[i]);
-
+nc += _strlen(av[i]);
 s = malloc(sizeof(char) * nc + 1);
 if (s == 0)
-	return (NULL);
-
+return (NULL);
 for (i = 0; i < ac; i++)
 {
-	for (j = 0; av[i][j] != '\0'; j++, cmpt++)
-		s[cmpt] = av[i][j];
-
-	s[cmpt] = '\n';
-	cmpt++;
+for (j = 0; av[i][j] != '\0'; j++, cmpt++)
+s[cmpt] = av[i][j];
+s[cmpt] = '\n';
+cmpt++;
 }
 s[cmpt] = '\0';
-
 return (s);
 }
