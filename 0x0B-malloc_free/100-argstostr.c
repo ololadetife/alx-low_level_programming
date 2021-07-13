6f9699a83444fd11da26ea89ca782322e06bef09
ololadetife <53858116+ololadetife@users.noreply.github.com>
2021-07-13 12:15:23 -0700

    Malloc and Free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,53 +1,52 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include "holberton.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: string s
- * Return: length of string
+ * _strlen - find length of a string
+ * @s: string
+ * Return: int
  */
+
+
 int _strlen(char *s)
 {
-	int len = 0;
-
-	while (*s++)
-		len++;
-
-	return (len);
+int size = 0;
+for (; s[size] != '\0'; size++)
+;
+return (size);
 }
 
 /**
- * argstostr - concatenates all the arguments of your program
- * @ac: argument count
- * @av: argument vector
- * Return: concatenated string
+ * *argstostr - description
+ * @ac: int
+ * @av: arguments
+ * Return: string
  */
+
 char *argstostr(int ac, char **av)
 {
-	int i, j, len, total;
-	int m = 0;
-	char *ptr;
+int i = 0, nc = 0, j = 0, cmpt = 0;
+char *s;
 
-	if (!ac || !av)
-		return (NULL);
-	total = 0;
-	for (i = 0; i < ac; i++)
-	{
-		len = _strlen(av[i]) + 1;
-		total += len;
-	}
-	ptr = malloc(sizeof(char) * total + 1);
-	if (!ptr)
-		return (NULL);
-	for (i = 0; i < ac; i++)
-	{
-		len = _strlen(av[i]);
-		for (j = 0; j < len; j++, m++)
-		{
-			ptr[m] = av[i][j];
-		}
-		ptr[m++] = '\n';
-	}
-	ptr[m] = '\0';
-	return (ptr);
+if (ac == 0 || av == NULL)
+	return (NULL);
+
+for (; i < ac; i++, nc++)
+	nc += _strlen(av[i]);
+
+s = malloc(sizeof(char) * nc + 1);
+if (s == 0)
+	return (NULL);
+
+for (i = 0; i < ac; i++)
+{
+	for (j = 0; av[i][j] != '\0'; j++, cmpt++)
+		s[cmpt] = av[i][j];
+
+	s[cmpt] = '\n';
+	cmpt++;
+}
+s[cmpt] = '\0';
+
+return (s);
 }
