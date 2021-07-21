f4130eca2028953e1c227fea4564be473bc3424e
ololadetife <53858116+ololadetife@users.noreply.github.com>
2021-07-21 11:04:30 +0100

    Function pointer

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,36 +1,30 @@
-#include <stddef.h>
-#include <stdlib.h>
 #include "3-calc.h"
+#include <string.h>
+
 /**
- * get_op_func - slect correct function
- * to be performed
- * @s: The operator
+ * get_op_func - selects the correct operation toperform
+ * @s: operation to perform
  *
- * Return: pointer to the operation function
+ * Return: pointer to the correct function
  */
-int (*get_op_func(char *s))(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
-op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
 	int i;
-	int j;
 
-	for (j = 0; s[j] != '\0'; j++)
-	;
-	if (j > 1)
-	return (NULL);
 	i = 0;
 	while (ops[i].op != NULL)
 	{
-	if (*s == *(ops[i].op))
-	break;
-	i++;
+		if (strcmp(s, ops[i].op) == 0)
+			break;
+		i++;
 	}
 	return (ops[i].f);
 }
