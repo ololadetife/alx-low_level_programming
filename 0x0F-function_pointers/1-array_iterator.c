fa68cb64e766ec8e8e74fd36a037214488e7fa71
ololadetife <53858116+ololadetife@users.noreply.github.com>
2021-07-21 10:44:37 +0100

    Function pointer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+
+#include <stddef.h>
+/**
+ * array_iterator - prints name using function pointer
+ * @array: the array
+ * @size: - size of the array
+ * @action: - function pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (size != 0 && action != 0 && array != 0)
+	{
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+	}
+}
